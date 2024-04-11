01b44af479f87580884dcc41d59b6d46eed93cc5
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-12 10:37:47 +1200

    add save + rerouting

--- client/pages/Recipes.tsx
+++ client/pages/Recipes.tsx
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom'
 import RecipeCardSml from '../components/RecipeCardSml'
 
 function Recipe() {
   return (
-    <div>
-      <div className="mb-24 mt-24 flex justify-center text-4xl">
+    <div className="mb-4 mt-24">
+      <div className="flex justify-center text-4xl">
         <h2>Pick Your Meals</h2>
       </div>
+      <div className="mt-2 flex justify-center">
+        <h3>Choose up to seven meals</h3>
+      </div>
       <RecipeCardSml />
+      <Link to="weekplan">Save and Go to meal plan</Link>
     </div>
   )
 }
