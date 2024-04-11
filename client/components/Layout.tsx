c819f7c5d5880fbcdd0bd6631ed29f7bede67572
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-11 21:40:37 +1200

    add Layout component + header + footer'

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+import Header from './Header'
 
 export default function Layout() {
   return (
-    <>
-      <header>
-        <h1>MealEasy</h1>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer></footer>
-    </>
+    <html lang="en">
+      <body className={' layout-body flex min-h-screen w-screen flex-col'}>
+        <div className="flex-1">
+          <Header />
+          <Outlet />
+        </div>
+        <Footer />
+      </body>
+    </html>
   )
 }
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,12 +1,12 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 
 import Landing from './pages/Landing'
-import Home from './pages/WeekPlan'
 import Profile from './pages/Profile'
 import EditProfile from './pages/EditProfile'
 import Recipes from './pages/Recipes'
 import Preferences from './pages/Preferences'
 import App from './components/App'
+import WeekPlan from './pages/WeekPlan'
 
 export const routes = createRoutesFromElements(
   <>
@@ -14,7 +14,7 @@ export const routes = createRoutesFromElements(
     <Route path="home" element={<App />}>
       <Route path="profile/:id" element={<Profile />} />
       <Route path="profile/id/edit" element={<EditProfile />} />
-      <Route path="weekplan" element={<Home />} />
+      <Route path="weekplan" element={<WeekPlan />} />
       <Route path="recipes" element={<Recipes />} />
       <Route path="preferences" element={<Preferences />} />
     </Route>
