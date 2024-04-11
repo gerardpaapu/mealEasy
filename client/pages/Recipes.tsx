19bdeb3af6720a8b32c88108ce5be6eb3ea2e3c3
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-11 23:20:17 +1200

    add recipe-card component

--- client/pages/Recipes.tsx
+++ client/pages/Recipes.tsx
@@ -1,5 +1,14 @@
+import RecipeCardSml from '../components/RecipeCardSml'
+
 function Recipe() {
-  return <h1>Recipe</h1>
+  return (
+    <div>
+      <div className="mb-24 mt-24 flex justify-center text-4xl">
+        <h2>Pick Your Meals</h2>
+      </div>
+      <RecipeCardSml />
+    </div>
+  )
 }
 
 export default Recipe
