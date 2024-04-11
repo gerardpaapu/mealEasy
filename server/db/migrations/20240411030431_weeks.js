3aa8c304fb39d134b673b1a9a80b50cc7d2d2ccc
giseleschaves <giseleschaves@gmail.com>
2024-04-11 15:31:05 +1200

    add recipes and weeks tables

---
+++ server/db/migrations/20240411030431_weeks.js
@@ -0,0 +1,18 @@
+export async function up(knex) {
+  await knex.schema.createTable('weeks', (table) => {
+    table.increments('id').primary()
+    table.integer('user_id').references('users.auth0_id')
+    table.integer('monday').references('recipes.id')
+    table.integer('tuesday').references('recipes.id')
+    table.integer('wednesday').references('recipes.id')
+    table.integer('thursday').references('recipes.id')
+    table.integer('friday').references('recipes.id')
+    table.integer('saturday').references('recipes.id')
+    table.integer('sunday').references('recipes.id')
+    table.integer('created')
+   })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('weeks')
+}
---
+++ server/db/seeds/5-weeks.js
@@ -0,0 +1,15 @@
+export async function seed(knex) {
+  await knex('weeks').insert([
+    {
+      user_id: 'auth0|6478f3fd75374ee3d7bc4d94',
+      monday: 5,
+      tuesday: 2,
+      wednesday: 4,
+      thursday: 3,
+      friday: 7,
+      saturday: 6,
+      sunday: 1,
+      created: '11/04/2023'
+    },
+  ])
+}
