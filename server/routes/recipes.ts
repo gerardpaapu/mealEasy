37ccc014b5f693c857a40b394ce50b33f6b80180
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2024-04-12 10:23:18 +1200

    add recipes routes

--- server/routes/recipes.ts
+++ server/routes/recipes.ts
@@ -1,14 +1,25 @@
 import express from 'express'
-import * as db from '../db/CRUD/user'
+import * as db from '../db/CRUD/recipes'
 
 const router = express.Router()
 export default router
 
 router.get('/:id', async (req, res) => {
   try {
-    const id = req.params.id
-    const user = await db.getUserById(id)
-    res.json(user)
+    const id = Number(req.params.id)
+    const recipe = await db.getRecipesById(id)
+    res.json(recipe)
+  } catch (e) {
+    res.status(500).send(`Error: API call not working, ${e}`)
+  }
+})
+
+router.get('/:name', async (req, res) => {
+  try {
+    const name = req.params.name
+
+    const recipe = await db.getRecipesByName(name)
+    res.json(recipe)
   } catch (e) {
     res.status(500).send(`Error: API call not working, ${e}`)
   }
@@ -17,19 +28,8 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const data = req.body
-    await db.addUsers(data)
-    res.status(200).send('user added')
-  } catch (e) {
-    res.status(500).send(`Error: API call not working, ${e}`)
-  }
-})
-
-router.patch('/:id', async (req, res) => {
-  try {
-    const id = req.params.id
-    const data = req.body
-    await db.updateUserById(id, data)
-    res.status(200).send('user updated')
+    await db.addRecipes(data)
+    res.status(200).send('Recipe added')
   } catch (e) {
     res.status(500).send(`Error: API call not working, ${e}`)
   }
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import * as Path from 'node:path'
 
-import fruitRoutes from './routes/fruits.ts'
+import users from './routes/users'
+import recipes from './routes/recipes'
 
 const server = express()
 
 server.use(express.json())
 
-// server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/users', users)
+server.use('/api/v1/recipes', recipes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
