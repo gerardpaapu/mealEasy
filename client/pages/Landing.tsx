02a686d7d44d8e9d9523f5dd58b92045b7b590ba
Katie Davies <millerkatie1990@gmail.com>
2024-04-11 16:11:39 +1200

    create landing

--- client/pages/Landing.tsx
+++ client/pages/Landing.tsx
@@ -1,5 +1,43 @@
 function Landing() {
-  return <h1>Landing</h1>
+  return (
+    <html lang="en">
+      <body className="layout-body min-h-screen w-screen">
+        <div className="flex justify-end">
+          {/* <Login />
+      <Register /> */}
+          <button>Sign up</button>
+          <button>Log in</button>
+        </div>
+        <div className=" flex flex-col  content-center justify-center">
+          <div className="flex justify-center">
+            <img
+              src="Public/images/blackLogo.png"
+              alt="MealEasy Logo"
+              className="w-80"
+            />
+          </div>
+          <div className="m-10 flex justify-center">
+            {/* <Register/> */}
+            <button> Sign Up</button>
+          </div>
+          <div className="flex justify-around">
+            <div>
+              <h1>image</h1>
+              <p>paragraph</p>
+            </div>
+            <div>
+              <h1>image</h1>
+              <p>paragraph</p>
+            </div>
+            <div>
+              <h1>image</h1>
+              <p>paragraph</p>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  )
 }
 
 export default Landing
