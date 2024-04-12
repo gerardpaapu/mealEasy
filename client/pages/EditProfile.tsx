6112e21ceb86efacef349903a98b345c60ab52bf
Katie Davies <millerkatie1990@gmail.com>
2024-04-12 15:30:26 +1200

    basic form setup with user details

--- client/pages/EditProfile.tsx
+++ client/pages/EditProfile.tsx
@@ -1,30 +1,66 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import Login from '../components/Login'
+import {
+  IfAuthenticated,
+  IfNotAuthenticated,
+} from '../components/Authenticated'
 
-const fakeData = {
-  auth0_id: 'auth0|648fd1c873375442becf2c60',
-  email: 'katie@example.com',
-  first_name: 'Katie',
-  last_name: 'Davies',
-  nickname: 'Katie',
-}
+// const fakeData = {
+//   auth0_id: 'auth0|648fd1c873375442becf2c60',
+//   email: 'katie@example.com',
+//   first_name: 'Katie',
+//   last_name: 'Davies',
+//   nickname: 'Katie',
+// }
 
 function EditProfile() {
-  const { user, isAuthenticated } = useAuth0()
-
-  isAuthenticated && (
-    <div>
-      <h1>Edit Your Profile</h1>
-      <form>
-        <input type="text" name="first_name" placeholder={user?.name} />
-      </form>
-    </div>
-  )
+  const { user } = useAuth0()
+  console.log(user)
 
   return (
     <>
-      <h2>Please login</h2>
-      <Login />
+      <IfAuthenticated>
+        {user && (
+          <div>
+            <h1>Edit Your Profile</h1>
+            <form>
+              <label htmlFor="nickname">
+                Nickname:
+                <input
+                  type="text"
+                  name="nickname"
+                  placeholder={user?.nickname}
+                />
+              </label>
+
+              <label htmlFor="first_name">
+                First Name:
+                <input type="text" name="first_name" placeholder={user?.name} />
+              </label>
+              <label htmlFor="last_name">
+                Last Name:
+                <input
+                  type="text"
+                  name="last_name"
+                  placeholder={user?.given_name}
+                />
+              </label>
+              <label htmlFor="email">
+                Email:
+                <input type="text" name="email" placeholder={user?.email} />
+              </label>
+              <button>Save</button>
+            </form>
+          </div>
+        )}
+      </IfAuthenticated>
+
+      <IfNotAuthenticated>
+        <>
+          <h2>Please login</h2>
+          <Login />
+        </>
+      </IfNotAuthenticated>
     </>
   )
 }
