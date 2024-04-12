aace402c0356222dfee1a74b69747d36816ab1bf
Katie Davies <millerkatie1990@gmail.com>
2024-04-12 15:12:09 +1200

    update form to show logged in user

--- client/pages/EditProfile.tsx
+++ client/pages/EditProfile.tsx
@@ -1,5 +1,32 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import Login from '../components/Login'
+
+const fakeData = {
+  auth0_id: 'auth0|648fd1c873375442becf2c60',
+  email: 'katie@example.com',
+  first_name: 'Katie',
+  last_name: 'Davies',
+  nickname: 'Katie',
+}
+
 function EditProfile() {
-  return <h1>EditProfile</h1>
+  const { user, isAuthenticated } = useAuth0()
+
+  isAuthenticated && (
+    <div>
+      <h1>Edit Your Profile</h1>
+      <form>
+        <input type="text" name="first_name" placeholder={user?.name} />
+      </form>
+    </div>
+  )
+
+  return (
+    <>
+      <h2>Please login</h2>
+      <Login />
+    </>
+  )
 }
 
 export default EditProfile
