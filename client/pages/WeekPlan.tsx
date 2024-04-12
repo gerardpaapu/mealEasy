83e915a18b4c26a6d6558943e7a25c07213a74ec
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-12 21:41:03 +1200

    fix heading for home component"

--- client/pages/WeekPlan.tsx
+++ client/pages/WeekPlan.tsx
@@ -33,35 +33,37 @@ export default function WeekPlan() {
   )
 
   return (
-    <div className="flex">
-      <div>
-        <h1 className="mb-4 text-center text-2xl font-bold">Your Week</h1>
-        <div className="flex flex-col items-start">
-          {daysOfWeek.map((day, index) => (
-            <div key={index} className="mb-10 flex h-36">
-              <div className="w-44">
-                <h2 className="text-xl font-semibold">{day}</h2>
-              </div>
-              <div className="card card-side h-28 w-80 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src="https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg"
-                    alt="Movie"
-                    className="h-12 w-10"
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title text-lg">Meal Name!</h2>
-                  <div className="card-actions justify-end">
-                    <button onClick={handleRecipeClick}>Recipe Detail</button>
+    <div>
+      <h1 className="mb-20 flex justify-center text-4xl">Your week</h1>
+      <div className="flex">
+        <div>
+          <div className="flex flex-col items-start">
+            {daysOfWeek.map((day, index) => (
+              <div key={index} className="mb-10 flex h-36">
+                <div className="w-44">
+                  <h2 className="text-xl font-semibold">{day}</h2>
+                </div>
+                <div className="card card-side h-28 w-80 bg-base-100 shadow-xl">
+                  <figure>
+                    <img
+                      src="https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg"
+                      alt="Movie"
+                      className="h-12 w-10"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title text-lg">Meal Name!</h2>
+                    <div className="card-actions justify-end">
+                      <button onClick={handleRecipeClick}>Recipe Detail</button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
+        <div className="ml-40">{selectedRecipe}</div>
       </div>
-      <div className="ml-40">{selectedRecipe}</div>
     </div>
   )
 }
