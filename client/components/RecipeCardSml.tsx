da6a586d595733ef2df23ba883b484d97bae0fed
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-12 12:22:08 +1200

    add function pick no more than 7 meals

--- client/components/RecipeCardSml.tsx
+++ client/components/RecipeCardSml.tsx
@@ -20,14 +20,47 @@ export default function RecipeCardSml() {
       image:
         'https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg',
     },
+    {
+      name: 'Butter Chicken',
+      image:
+        'https://img.taste.com.au/sL0izJWj/w643-h428-cfill-q90/taste/2016/11/butter-chicken-101831-1.jpeg',
+    },
+    {
+      name: 'Spaghetti Carbonara',
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/carbonara-index-6476367f40c39.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*',
+    },
+    {
+      name: 'Sushi',
+      image:
+        'https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg',
+    },
+    {
+      name: 'Butter Chicken',
+      image:
+        'https://img.taste.com.au/sL0izJWj/w643-h428-cfill-q90/taste/2016/11/butter-chicken-101831-1.jpeg',
+    },
+    {
+      name: 'Spaghetti Carbonara',
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/carbonara-index-6476367f40c39.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*',
+    },
+    {
+      name: 'Sushi',
+      image:
+        'https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg',
+    },
   ]
 
   const handleSelectMeal = (index) => {
-    setSelectedMeal(index)
-    if (selectedItems.includes(index)) {
-      setSelectedItems(selectedItems.filter((item) => item !== index)) //if there is index in the array - remove it
-    } else {
-      setSelectedItems([...selectedItems, index]) //if the index is not in the array - add it
+    if (selectedItems.length < 7 || selectedItems.includes(index)) {
+      //if selected items less than 7 or includes index
+      setSelectedMeal(index)
+      if (selectedItems.includes(index)) {
+        setSelectedItems(selectedItems.filter((item) => item !== index)) // remove the index if it's already selected
+      } else {
+        setSelectedItems([...selectedItems, index]) // add the index if it's not selected
+      }
     }
   }
 
