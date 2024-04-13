151c347061682899bd3e2230f77d4afe2c8103a4
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-13 16:39:32 +1200

    fix typescript errors for week plan

--- client/pages/WeekPlan.tsx
+++ client/pages/WeekPlan.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 import RecipeDetail from '../components/RecipeDetailCard'
 
@@ -13,7 +13,7 @@ export default function WeekPlan() {
     'Sunday',
   ]
 
-  const [selectedRecipe, setSelectedRecipe] = useState(null)
+  const [selectedRecipe, setSelectedRecipe] = useState<ReactNode | null>(null)
 
   const handleRecipeClick = () => {
     setSelectedRecipe(
@@ -28,21 +28,21 @@ export default function WeekPlan() {
 
   return (
     <div>
-      <h1 className="text-headingGreen mb-20 flex justify-center text-4xl">
+      <h1 className="text-headingGreen mb-14 flex justify-center text-4xl">
         Your week
       </h1>
-      <div className="flex">
+      <div className="mb-20 flex">
         <div>
           <div className="ml-12 flex flex-col items-start">
             {daysOfWeek.map((day, index) => (
-              <div key={index} className="mb-10 h-36">
-                <h2 className="text-headingGreen text-xl font-semibold">
+              <div key={index} className="h-32">
+                <h2 className="text-headingGreen mb-1 text-xl font-semibold">
                   {day}
                 </h2>
-                <div className="card card-side h-28 w-80 bg-base-100 bg-white shadow-xl">
-                  <div className="card-body">
-                    <h2 className="card-title text-lg">Meal Name!</h2>
-                    <div className="card-actions justify-end">
+                <div className="card card-side h-20 w-80 bg-white shadow-xl">
+                  <div>
+                    <h2 className="card-title ml-2 mt-2 text-lg">Meal Name!</h2>
+                    <div>
                       <button onClick={handleRecipeClick}>Recipe Detail</button>
                     </div>
                   </div>
