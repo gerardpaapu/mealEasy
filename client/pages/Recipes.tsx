5f03d9f9da3a5064b374ea3d540d82ea320a391c
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-13 21:22:36 +1200

    small changes in styling

--- client/pages/Recipes.tsx
+++ client/pages/Recipes.tsx
@@ -4,18 +4,22 @@ import Button from '../components/Button'
 
 function Recipe() {
   return (
-    <div className="flex h-full items-center justify-center">
-      <div className="mb-4 ml-16">
+    <div>
+      <div>
         <div className="flex justify-center text-4xl">
           <h2 className="text-headingGreen">Pick Your Meals</h2>
         </div>
         <div className="mb-10 mt-2 flex justify-center">
           <h3>Choose up to seven meals</h3>
         </div>
-        <RecipeCardMedium />
-        <Link to="/home">
-          <Button>Save and See your plan</Button>
-        </Link>
+      </div>
+      <div className="flex h-full items-center justify-center">
+        <div className="mb-4 ml-16">
+          <RecipeCardMedium />
+          <Link to="/home">
+            <Button>Save and See your plan</Button>
+          </Link>
+        </div>
       </div>
     </div>
   )
