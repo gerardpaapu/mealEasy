4bc84d6e8f769c50e98f8ae491a5ceaceb3c2b65
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-13 14:48:41 +1200

    create recipe detail component

--- client/pages/WeekPlan.tsx
+++ client/pages/WeekPlan.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import RecipeDetail from '../components/RecipeDetailCard'
 
 export default function WeekPlan() {
   const daysOfWeek = [
@@ -15,23 +16,15 @@ export default function WeekPlan() {
   const [selectedRecipe, setSelectedRecipe] = useState(null)
 
   const handleRecipeClick = () => {
-    setSelectedRecipe(selectedRecipe === null ? recipeDetail : null)
-  }
-
-  const recipeDetail = (
-    <div className="card w-96 bg-white shadow-xl">
-      <figure>
-        <img
-          src="https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg"
-          alt="Shoes"
+    setSelectedRecipe(
+      selectedRecipe === null ? (
+        <RecipeDetail
+          imageUrl="https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg"
+          recipeName="Recipe Name"
         />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Recipe Name</h2>
-        <p>More details</p>
-      </div>
-    </div>
-  )
+      ) : null,
+    )
+  }
 
   return (
     <div>
@@ -43,19 +36,10 @@ export default function WeekPlan() {
           <div className="ml-12 flex flex-col items-start">
             {daysOfWeek.map((day, index) => (
               <div key={index} className="mb-10 h-36">
-                {/* <div className="w-44"> */}
                 <h2 className="text-headingGreen text-xl font-semibold">
                   {day}
                 </h2>
-                {/* </div> */}
                 <div className="card card-side h-28 w-80 bg-base-100 bg-white shadow-xl">
-                  <figure>
-                    <img
-                      src="https://img.taste.com.au/3mYHXsD_/taste/2016/11/sushi-for-kids-81300-1.jpeg"
-                      alt="Movie"
-                      className="h-12 w-10"
-                    />
-                  </figure>
                   <div className="card-body">
                     <h2 className="card-title text-lg">Meal Name!</h2>
                     <div className="card-actions justify-end">
