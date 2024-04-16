d06cc974618b95c0a695e27a105d719d9e33bc0b
Katie Davies <millerkatie1990@gmail.com>
2024-04-16 13:05:33 +1200

    clean up file

--- client/components/Logout.test.tsx
+++ client/components/Logout.test.tsx
@@ -4,9 +4,6 @@ import { describe, it, expect } from 'vitest'
 
 import { renderComponent } from '../test-utils'
 import LogoutButton from './Logout'
-import { BrowserRouter } from 'react-router-dom'
-import userEvent from '@testing-library/user-event'
-import { Auth0Provider } from '@auth0/auth0-react'
 
 describe('Logout Button', () => {
   it('should render a log out button', async () => {
