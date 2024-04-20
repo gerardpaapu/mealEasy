ef1d8a8eb0464269aed18f69420a8e0662293c09
Barbora Klusackova <barboraklusackova@gmail.com>
2024-04-20 23:07:01 +1200

    add cursor pointer when hovering over magnif glass

--- client/components/RecipeCardMedium.tsx
+++ client/components/RecipeCardMedium.tsx
@@ -191,7 +191,7 @@ export default function RecipeCardMedium() {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
               fill="currentColor"
-              className="h-4 w-4 opacity-70"
+              className="h-4 w-4 cursor-pointer opacity-70"
               onClick={() => handleFilter()}
             >
               <path
